ae4bf2de513ba6cbd8b1a29e2e46178879ca97f2
syntax-project36 <jattooseze09@gmail.com>
2026-07-24 12:39:41 +0100

    Allow HEAD requests on keep_alive endpoint for UptimeRobot

--- keep_alive.py
+++ keep_alive.py
@@ -3,9 +3,9 @@ from threading import Thread
 
 app = Flask('')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'HEAD'])
 def home():
-    return "Bot is alive!"
+    return "I'm alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
